36b956297bc9d57e1ba675b3022afb177f64ecc8
Skye Chen <skye.chen@statcan.gc.ca>
2022-08-02 22:51:24 +0000

    Added documentation and improved data integrity verification

--- 1-PreProcessing/raw/compress_vancouver/compress_vancouver.py
+++ 1-PreProcessing/raw/compress_vancouver/compress_vancouver.py
@@ -1,14 +1,39 @@
+'''
+compress_vancouver.py
+
+Compresses the Vancouver dataset located in raw/ to allow it to be stored on GitHub
+Store the hash to a txt file
+'''
+
 import zlib
+import hashlib 
+sha256 = hashlib.sha256()
 
+# Define file paths
 filename = 'BC_Vancouver_Business_Licences'
-input_file = f'/home/jovyan/NewServer/ODBiz/1-PreProcessing/raw/{filename}.csv'
-output_dir = f'/home/jovyan/NewServer/ODBiz/1-PreProcessing/raw/compress_vancouver'
+input_file = f'/home/jovyan/ODBiz/1-PreProcessing/raw/{filename}.csv'
+output_dir = f'/home/jovyan/ODBiz/1-PreProcessing/raw/compress_vancouver'
+hash_dir = f'{output_dir}/van_original_sha256_hash.txt'
 
+# Read the Vancouver dataset
 with open(input_file, mode = 'rb') as fp:
     data = fp.read()
     comp_fp = zlib.compress(data)
 
+# Compress the dataset
 output_filename = f'{output_dir}/{filename}.zip'
 with open(output_filename, mode = 'wb') as fp:
     fp.write(comp_fp)
     print(f'Saved archive to {output_filename}')
+
+# Record the sha256 hash of the csv for data verification
+with open(input_file, mode = 'rb') as fp:
+    data = fp.read()
+    sha256.update(data)
+van_original_sha256_hash = sha256.hexdigest()
+
+# Save the hash to a txt file
+with open(hash_dir, mode = 'w') as fp:
+    fp.write(van_original_sha256_hash)
+    print(f'The sha256 hash of the archive is: {van_original_sha256_hash}')
+    print(f'Saved hash to {hash_dir}')
--- 1-PreProcessing/raw/compress_vancouver/decompress_vancouver.py
+++ 1-PreProcessing/raw/compress_vancouver/decompress_vancouver.py
@@ -1,27 +1,44 @@
+'''
+decompress_vancouver.py
+
+Decompresses the Vancouver dataset from the Git repo
+Compares the hash of the new csv file to the hash of the original csv to ensure
+that no data was lost.
+'''
+
 import zlib
 import hashlib 
 sha256 = hashlib.sha256()
-van_original_sha256_hash = '634bd8f6378968c610d7afa5bad7f80e17701d5d1ebc605be3e89849217a2392'
 
+# Define file paths
 wkdir = '/home/jovyan/ODBiz/1-PreProcessing/raw/'
 filename = 'BC_Vancouver_Business_Licences'
 input_file = f'{wkdir}compress_vancouver/{filename}.zip'
 output_dir = f'{wkdir}{filename}.csv'
+hash_dir = f'{wkdir}compress_vancouver/van_original_sha256_hash.txt'
 
+# Read the zip archive
 with open(input_file, mode = 'rb') as fp:
     data = fp.read()
     decomp_fp = zlib.decompress(data)
 
+# Extract the zip archive contents to a csv
 output_filename = output_dir
 with open(output_filename, mode = 'wb') as fp:
     fp.write(decomp_fp)
     print(f'Extracted to {output_filename}')
 
+# Calculate the hash of the extracted csv
 with open(output_filename, mode = 'rb') as fp:
     data = fp.read()
     sha256.update(data)
-
 new_hash = sha256.hexdigest()
+
+# Retrieve the hash of the original csv
+with open(hash_dir) as fp:
+    van_original_sha256_hash = fp.read()
+
+# Compare the hashes to ensure it's the exact same file
 if new_hash == van_original_sha256_hash:
     print('The extracted file hash matches the original hash! Extraction successful!')
     print(f'SHA256: {van_original_sha256_hash}')
